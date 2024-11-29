b77b1799e31688814c2f1518f3ccd25b671ce490
Dan Muckerman <danielmuckerman@me.com>
2024-11-29 01:22:17 -0500

    Lose money proportional to badges and level

--- engine/overworld/scripting.asm
+++ engine/overworld/scripting.asm
@@ -1192,6 +1192,8 @@ Script_reloadmapafterbattle:
 	ld hl, wBattleScriptFlags
 	ld d, [hl]
 	ld [hl], 0
+	ld hl, wWildBattlePanic
+	ld [hl], d
 	ld a, [wBattleResult]
 	and ~BATTLERESULT_BITMASK
 	cp LOSE
--- ram/wram.asm
+++ ram/wram.asm
@@ -2889,7 +2889,7 @@ wScriptTextBank::
 wDeferredScriptAddr::
 wScriptTextAddr::
 	dw
-	ds 1
+wWildBattlePanic:: db
 wWildEncounterCooldown:: db
 
 wXYComparePointer:: dw
